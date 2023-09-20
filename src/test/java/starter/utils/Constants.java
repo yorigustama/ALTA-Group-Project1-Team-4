b7f0398f39b034e4a471d77cf0775235a1df40b0
yorigustama <yfiandika@gmail.com>
2023-09-20 19:01:42 +0700

    Add File Constants.java

---
+++ src/test/java/starter/utils/Constants.java
@@ -0,0 +1,10 @@
+package starter.utils;
+
+public class Constants {
+
+    public final String BASE_URL = "https://dummyjson.com";
+
+    public static String DIR = System.getProperty("user.dir");
+    public static String JSON = DIR+"/src/test/resources/JSON";
+
+}
